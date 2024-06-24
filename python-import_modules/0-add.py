3d797785a4e0a324619328401736f2a7a2782bf7
Ojotule <o.attah@alustudent.com>
2024-06-24 04:43:33 -0700

    okay

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def add(a, b):
-    """ My addition function
- Args:
-        a:40
-        b:60
-  Returns:
-        The return value. a + b
-    """
     return (a+b)
-
+from add_0 import add
+a=1
+b=2
+print("{} + {} = {}".format(a, b, result))
