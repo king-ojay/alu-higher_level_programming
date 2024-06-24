4309d0d89e09f7960a270d49c00f8c02d280943c
Ojotule <o.attah@alustudent.com>
2024-06-24 08:49:56 -0700

    okay

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
+
+empty_list = []
+max_value = max_integer(empty_list)
+print("Max: {}".format(max_value))
