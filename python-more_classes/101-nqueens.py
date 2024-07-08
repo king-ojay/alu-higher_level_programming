221d80ed44cae2a37c73d1e9e1eacbfb8f2931ef
Ojotule <o.attah@alustudent.com>
2024-07-08 11:55:28 -0700

    finisheddd

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    # Check this row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
+
+def solve_nqueens(board, col):
+    if col >= len(board):
+        print_solution(board)
+        return True
+    
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+    
+    return res
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] ==
+
