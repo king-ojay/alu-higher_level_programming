bbbde925568303dbd9d0463da81b5875a4ae6946
Ojotule <o.attah@alustudent.com>
2024-07-08 12:17:17 -0700

    okay

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -2,35 +2,27 @@
 import sys
 
 def is_safe(board, row, col):
-    # Check this row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-    
-    # Check upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
-    # Check lower diagonal on the left side
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    
     return True
 
 def solve_nqueens(board, col):
     if col >= len(board):
         print_solution(board)
         return True
-    
     res = False
     for i in range(len(board)):
         if is_safe(board, i, col):
             board[i][col] = 1
             res = solve_nqueens(board, col + 1) or res
             board[i][col] = 0
-    
     return res
 
 def print_solution(board):
@@ -39,3 +31,22 @@ def print_solution(board):
         for j in range(len(board)):
             if board[i][j] == 1:
                 solution.append([i, j])
+    print(solution)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0)
+
+if __name__ == "__main__":
+    main()
