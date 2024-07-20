17c5645589f0420edd3d91277d5097c654773717
Ojotule <o.attah@alustudent.com>
2024-07-19 18:58:25 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list,
-and then save them to a file"""
+"""
+This module adds all arguments to a Python list and saves them to a file.
+
+The list is saved as a JSON representation in a file named add_item.json.
+"""
+
 import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+def main():
+    """
+    Main function to add all command line arguments to a list and save to a file.
+    """
+    filename = 'add_item.json'
 
+    # Load existing data from the file, if it exists
     try:
-        items = load_from_json_file("add_item.json")
+        items = load_from_json_file(filename)
     except FileNotFoundError:
         items = []
+
+    # Add new items from command line arguments
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+
+    # Save the updated list back to the file
+    save_to_json_file(items, filename)
+
+if __name__ == '__main__':
+    main()
