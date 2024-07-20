e805ba4b160afcb2d3e87d1308d67e6111a7855d
Ojotule <o.attah@alustudent.com>
2024-07-19 18:19:43 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+This script adds all arguments to a Python list, and then saves them to a file.
+"""
+
 import sys
 import os
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
