15d913c7fa94d662a2c5ae01c9b1bebfaf727a2d
Ojotule <o.attah@alustudent.com>
2024-07-27 15:48:52 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,13 @@
--- my privileges
+-- Check and show privileges for user_0d_1
+SELECT IF(COUNT(*) = 1, 'Exists', 'Does not exist') AS user_status
+FROM mysql.user
+WHERE user = 'user_0d_1' AND host = 'localhost';
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check and show privileges for user_0d_2
+SELECT IF(COUNT(*) = 1, 'Exists', 'Does not exist') AS user_status
+FROM mysql.user
+WHERE user = 'user_0d_2' AND host = 'localhost';
+
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
