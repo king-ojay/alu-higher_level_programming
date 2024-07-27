ced0906f7dad98d41c5bd5985b01bb30d3702251
Ojotule <o.attah@alustudent.com>
2024-07-27 15:17:39 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,3 @@
--- Create the users if they do not already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to the users
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush the privileges to ensure that they are saved and available in the current session
-FLUSH PRIVILEGES;
-
--- List the privileges for user_0d_1
+-- my privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List the privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
