2e5b89fc9753fb26192affa384dd19bf69e1116d
root <o.attah@alustudent.com>
2024-07-29 11:03:12 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
- https://intranet.hbtn.io/status."""
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
-
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
