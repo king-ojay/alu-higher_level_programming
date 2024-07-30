079fb39f465ec7cc94d2aef93a956b180156a3f2
root <o.attah@alustudent.com>
2024-07-30 13:50:09 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://alu-intranet.hbtn.io/status
-"""
 import urllib.request
 
+url = 'https://alu-intranet.hbtn.io/status'
+request = urllib.request.Request(url)
 
-if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+with urllib.request.urlopen(request) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
