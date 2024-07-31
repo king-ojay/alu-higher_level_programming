c3fd7bff7be2a352e7165dc2c61b69f6d034d446
Ojotule <o.attah@alustudent.com>
2024-07-31 08:25:21 -0700

    database_setup.py

--- grade-book-app_Ojotule_Attah/database_setup.py
+++ grade-book-app_Ojotule_Attah/database_setup.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, Student, Course
+from models import Base
 
 engine = create_engine('sqlite:///gradebook.db')
 Base.metadata.create_all(engine)
