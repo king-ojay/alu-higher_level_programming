80b6cfb922882c4f9ddfc126bc57e307474608a0
Ojotule <o.attah@alustudent.com>
2024-07-31 08:17:52 -0700

    databse_setup.py

--- grade-book-app_Ojotule_Attah/databse_setup.py
+++ grade-book-app_Ojotule_Attah/databse_setup.py
@@ -1,14 +1,10 @@
-# database_setup.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from models import Base, Student, Course
 
-# Create an SQLite database
 engine = create_engine('sqlite:///gradebook.db')
 Base.metadata.create_all(engine)
 
-# Create a configured "Session" class
 Session = sessionmaker(bind=engine)
-# Create a session
 session = Session()
 
