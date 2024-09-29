b09ba76d1222847ecefe6ccd71d7ab6bf3fa6cd7
Ojotule <o.attah@alustudent.com>
2024-09-29 14:57:17 -0700

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,20 +1,32 @@
-
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object if w or h is 0 or not a positive integer
+      return {};
+    }
+    this.width = w;  // Initialize the width
+    this.height = h; // Initialize the height
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width)); // Print the rectangle using 'X'
+    }
   }
 
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
+  rotate() {
+    const temp = this.width;  // Store the width in a temporary variable
+    this.width = this.height;  // Swap width and height
+    this.height = temp;        // Assign the stored width to height
   }
 
-  double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+  double() {
+    this.width *= 2;           // Double the width
+    this.height *= 2;          // Double the height
   }
-};
+}
+
+module.exports = Rectangle; // Export the Rectangle class
 
